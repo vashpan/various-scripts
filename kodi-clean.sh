15937be61e04e6de2ca060e58fb559fa1059162e
Konrad Kołakowski <vashpan@gmail.com>
2016-03-13 12:34:41 +0100

    Removed Kodi scripts, no longer needed.

--- kodi-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# clean non-existent content
-curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Clean", "id": "mybash"}' -H 'content-type: application/json;' http://150.100.0.150:8080/jsonrpc
-sleep 1
-curl --data-binary '{ "jsonrpc": "2.0", "method": "AudioLibrary.Clean", "id": "mybash"}' -H 'content-type: application/json;' http://150.100.0.150:8080/jsonrpc
-sleep 1
